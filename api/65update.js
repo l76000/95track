d1fa4321b33490073b9e334dd869b55918b3b4b3
l76000 <colakovicl035@gmail.com>
2025-11-02 22:18:57 +0100

    Create 65update.js

---
+++ api/65update.js
@@ -0,0 +1,15 @@
+import { runUpdateAndReturnData } from './_utilss.js';
+
+export default async function handler(request, response) {
+    try {
+        // Pokreni logiku ažuriranja
+        await runUpdateAndReturnData();
+        
+        console.log("Cron update je uspešno završen.");
+        response.status(200).json({ message: "Cron update završen." });
+    
+    } catch (error) {
+        console.error("Greška u run-update cron-u:", error);
+        response.status(500).json({ error: error.message });
+    }
+}
